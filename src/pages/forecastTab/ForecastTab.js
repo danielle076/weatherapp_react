fa5e53364b66af036fb0dcc7414d060b5b21e035
danielle076 <d.vandenakker@novi-education.nl>
2021-03-07 15:47:58 +0100

    gebruikersfeedback laadtijden

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -2,11 +2,12 @@ import React, {useEffect, useState} from 'react';
 import './ForecastTab.css';
 import axios from 'axios';
 
-const apiKey = "8ede14718d39d8e9ca1aacb5a13cfde7";
+const apiKey = "";
 
 function ForecastTab({ coordinates }) {
     const [forecasts, setForecasts] = useState(null);
     const [error, setError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
 
     function createDateString(timestamp) {
         const day = new Date(timestamp * 1000);
@@ -16,13 +17,16 @@ function ForecastTab({ coordinates }) {
     useEffect(() => {
         async function fetchData() {
             setError(false);
+            toggleLoading(true);
 
             try {
                 const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates?.lat}&lon=${coordinates?.lon}&exclude=minutely,current,hourly&appid=${apiKey}&lang=nl`);
                 setForecasts(result.data.daily.slice(1, 6));
+                toggleLoading(false);
             } catch (e) {
                 console.error(e);
                 setError(true);
+                toggleLoading(false);
             }
         }
 
@@ -59,6 +63,8 @@ function ForecastTab({ coordinates }) {
             )}
 
             {error && <span>Er is iets misgegaan met het ophalen van de data.</span>}
+
+            {loading && (<span>Loading...</span>)}
         </div>
     );
 };
