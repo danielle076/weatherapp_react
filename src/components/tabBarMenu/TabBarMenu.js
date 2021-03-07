7c21684b4110fff93c5c6b31c9c0537f1cef3880
danielle076 <d.vandenakker@novi-education.nl>
2021-03-07 16:09:51 +0100

    tabbladen

--- src/components/tabBarMenu/TabBarMenu.js
+++ src/components/tabBarMenu/TabBarMenu.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './TabBarMenu.css';
 
 function TabBarMenu() {
   return (
-    <nav className="tab-bar">
-      <ul>
-        <li>
-          <a className="active" href="/">
-            Vandaag
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            Komende week
-          </a>
-        </li>
-      </ul>
-    </nav>
+      <nav className="tab-bar">
+        <ul>
+          <li>
+            <NavLink activeClassName="active" exact to="/">
+              Vandaag
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/komende-week">
+              Komende week
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
   );
 };
 
